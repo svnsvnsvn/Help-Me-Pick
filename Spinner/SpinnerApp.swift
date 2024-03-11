49b3f82ce0214b61e32f75550439c70ec972ed70
Ian <Annubaka223@gmail.com>
2024-03-11 18:13:14 -0400

    Initial Commit

---
+++ Spinner/SpinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinnerApp.swift
+//  Spinner
+//
+//  Created by Ann Ubaka on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SpinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
